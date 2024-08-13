9bd1c00a2150df0c945de890c4ae118da3771047
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-13 20:16:25 -0300

    exercicio03

---
+++ exercicio03.js
@@ -0,0 +1,10 @@
+let n1=10;
+let n2=7;
+let n3=8.5;
+let n4=9;
+
+let media=(n1 + n2 + n3 + n4) / 4;
+
+console.log(`nota1=${n1} nota2=${n2} nota3=${n3} nota4=${n4}`);
+console.log(`media=${media}`);
+
